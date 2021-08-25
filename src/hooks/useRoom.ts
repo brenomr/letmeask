9810ef7b2b7648488579279cdd547259171aac7c
Breno <brenocartoon@hotmail.com>
2021-08-24 21:57:05 -0300

    Atualizar hook de salas Permitir retorno de contagem e id dos likes para manipulação deles

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type QuestionType = {
   id: string;
@@ -10,6 +11,8 @@ type QuestionType = {
   content: string;
   isHighlighted: string;
   isAnswered: string;
+  likecount: number;
+  likeId: string | undefined;
 }
 
 type FirebaseQuestions = Record<string, {
@@ -20,9 +23,13 @@ type FirebaseQuestions = Record<string, {
   content: string;
   isHighlighted: string;
   isAnswered: string;
+  likes: Record<string, {
+    authorId: string;
+  }>
 }>
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState();
 
@@ -40,13 +47,20 @@ export function useRoom(roomId: string) {
           author: value.author,
           isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
+          likecount: Object.values(value.likes ?? {}).length,
+          // hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id),
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
         }
       });
 
       setTitle(dbRoom.title);
       setQuestions(parsedQuestion);
     });
-  }, [roomId]);
+
+    return () => {
+      roomRef.off('value');
+    }
+  }, [roomId, user?.id]);
 
   return { questions, title }
 }
